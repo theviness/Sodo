64de9587e326e7db21fd3de1def414278030e627
1760241 <luuthienvi.github@gmail.com>
2023-11-10 22:15:37 +0700

    Initial commit

---
+++ src/main/java/com/luuthienvi/sodo/SodoApplication.java
@@ -0,0 +1,13 @@
+package com.luuthienvi.sodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luuthienvi/sodo/SodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luuthienvi.sodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
